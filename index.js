eac7df5920892e751aa7d3aaab261cfe98ee95e3
NeRo <99098290+NeRo707@users.noreply.github.com>
2024-01-18 15:08:44 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,6 @@ app.get('/', function (request, response) {
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  // Allow preflight requests
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      return res.status(200).json({});
-  }
-
   next();
 });
 
